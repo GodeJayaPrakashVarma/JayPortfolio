c47da2e8ddc33482924013a084a5b6d81782f846
Gode Jaya Prakash Varma <gjayaprakashvarma@gmail.com>
2025-06-13 15:04:26 +0530

    Fix Prettier formatting issues

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import { ThemeProvider } from './contexts/theme';
-import './index.css';
+import {createRoot} from 'react-dom/client'
+import App from './App'
+import {ThemeProvider} from './contexts/theme'
+import './index.css'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const container = document.getElementById('root')
 
-root.render(
+createRoot(container).render(
   <ThemeProvider>
     <App />
-  </ThemeProvider>
-);
+  </ThemeProvider>,
+)
